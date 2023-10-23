102c2e9660c631c7144bba44e70062f83b209ffe
Madhava Reddy <92641734+Madhava20@users.noreply.github.com>
2023-10-23 11:22:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import WordCounter from './wordCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* Other content */}
+        <WordCounter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
